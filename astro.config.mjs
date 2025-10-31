6279e31baac16281bdb8bd543de725c050d83c49
Ke Liu <liukeyla@umich.edu>
2025-10-30 20:10:42 -0400

    Setup Github Pages deployment

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://keeeeeliu.github.io',
+  // No base path needed when repo is named 'keeeeeliu.github.io'
+  integrations: [mdx()]
+});
